e050ec86f5f126e22772db58188ab1d02ce1ebb4
Mudasir Najar <mudasir1bashir@gmail.com>
2023-12-04 11:51:19 +0530

    Pizza Bill Generator App Added

---
+++ src/com/driver/Main.java
@@ -0,0 +1,24 @@
+package com.driver;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Example 1:
+Base Price Of The Pizza: 300
+Extra Cheese Added: 80
+Extra Toppings Added: 70
+Paperbag Added: 20
+Total Price: 470
+         */
+        Pizza pizza1 = new Pizza(true);
+        pizza1.addExtraCheese();
+        pizza1.addExtraToppings(true);
+        pizza1.addTakeAway();
+        pizza1.totalBill();
+
+        DeluxePizza pizza2 = new DeluxePizza(false);
+        pizza2.totalBill();
+
+    }
+}
---
+++ src/com/driver/Pizza.java
@@ -0,0 +1,77 @@
+package com.driver;
+
+public class Pizza {
+
+    // regular and deluxe pizza and both have veg and non-veg variants
+
+    /*
+    Veg pizza base price = 300
+Non-veg pizza base price = 400
+    Extra Cheese Price = 80
+Extra Toppings For Veg Pizza = 70
+Extra Toppings For Non-veg Pizza = 120
+Paper bag Price = 20
+     */
+    private int price;
+    private boolean isVeg;
+    private String bill;
+    private boolean extraCheese;
+    private boolean extraToppings;
+    private boolean takeAway;
+
+    public Pizza(boolean isVeg) {
+        this.isVeg = isVeg;
+        if (isVeg) {
+            this.price = 300;
+        } else {
+            this.price = 400;
+        }
+    }
+
+    public void addExtraCheese() {
+        this.price += 80;
+        extraCheese = true;
+    }
+
+    public void addExtraToppings(boolean isVeg) {
+        extraToppings = true;
+        this.isVeg = isVeg;
+        if (isVeg) {
+            this.price += 70;
+        } else {
+            this.price += 120;
+        }
+
+    }
+
+    public void addTakeAway() {
+        this.price += 20;
+        takeAway = true;
+    }
+
+    public void totalBill() {
+        if (isVeg) {
+            System.out.println("Base Price Of The Pizza: 300");
+        } else {
+            System.out.println("Base Price Of The Pizza: 400");
+        }
+
+        if (extraCheese) {
+            System.out.println("Extra Cheese Added: 80");
+        }
+        if (extraToppings) {
+            if (isVeg) {
+                System.out.println("Extra Toppings Added = 70");
+            } else {
+                System.out.println("Extra Toppings Added = 120");
+            }
+        }
+        if (takeAway) {
+            System.out.println("Paperbag Added = 20");
+        }
+        System.out.println("Total Price: " + price);
+    }
+
+
+}
+
